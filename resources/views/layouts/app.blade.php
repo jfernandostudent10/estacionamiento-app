517c2a1eef33ab6c8f74de0ead4ded7076aadc3b
Jose Huancilla <jfernandostudent10@gmail.com>
2024-10-27 12:45:00 -0500

    update app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,147 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="es" data-footer="true">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>UTP+ Estacionamiento| {{ $title ?? ''}}</title>
+    <meta name="description" content="{{ $description ?? ''}}"/>
+    <!-- Favicon Tags Start -->
+    @include('_layout.head')
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+<div id="root">
+    <div id="nav" class="nav-container d-flex">
+        <div class="nav-content d-flex">
+            <!-- Logo Start -->
+            <div class="logo position-relative">
+                <a href="{{ route('home') }}">
+                    <!-- Logo can be added directly -->
+                    <!-- <img src="img/logo/logo-white.svg" alt="logo" /> -->
+
+                    <!-- Or added via css to provide different ones for different color themes -->
+                    <img src="{{ asset('img/favicon-v2.ico') }}" alt="" style="width: 40px">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            </div>
+            <!-- Logo End -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+            <!-- User Menu Start -->
+            <div class="user-container d-flex">
+                <a href="#" class="d-flex user position-relative" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="profile" alt="profile" src="https://class.utp.edu.pe/static/media/student.42525dea.svg" />
+                    <div class="name text-black">
+                        {{ Auth::user()->name }} <br>
+                        <span class="mt-1">
+                            {{ Auth::user()->getCurrentRoles() }}
+                        </span>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end user-menu wide">
+                    <div class="row mb-1 ms-0 me-0">
+                        <div class="col-12 pe-1 ps-1">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        <i data-acorn-icon="logout" class="me-2" data-acorn-size="17"></i>
+                                        <span class="align-middle">Cerrar sesion</span>
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
+            <!-- User Menu End -->
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            <!-- Icons Menu Start -->
+            {{--<ul class="list-unstyled list-inline text-center menu-icons">
+                <li class="list-inline-item">
+                    <a href="#" id="colorButton" style="color: var(--dark-text-darker)">
+                        <i data-acorn-icon="light-on" class="light" data-acorn-size="18"></i>
+                        <i data-acorn-icon="light-off" class="dark" data-acorn-size="18"></i>
+                    </a>
+                </li>
+
+            </ul>--}}
+            <!-- Icons Menu End -->
+
+            <!-- Menu Start -->
+            <div class="menu-container flex-grow-1">
+                <ul id="menu" class="menu">
+
+                </ul>
+            </div>
+            <!-- Menu End -->
+
+            <!-- Mobile Buttons Start -->
+            <div class="mobile-buttons-container">
+                <!-- Menu Button Start -->
+                <a href="#" id="mobileMenuButton" class="menu-button">
+                    <i data-acorn-icon="menu"></i>
+                </a>
+                <!-- Menu Button End -->
+            </div>
+            <!-- Mobile Buttons End -->
+        </div>
+        <div class="nav-shadow"></div>
     </div>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <!-- Menu Start -->
+                <div class="col-auto d-none d-lg-flex">
+                    <ul class="sw-25 side-menu mb-0 primary" id="menuSide">
+                        <li>
+                            <a href="#" data-bs-target="#dashboard">
+                                <i data-acorn-icon="home" class="icon" data-acorn-size="18"></i>
+                            </a>
+                            <ul>
+                                <li>
+                                    <a href="Dashboard.GettingStarted.html">
+                                        <i data-acorn-icon="navigate-diagonal" class="icon d-none" data-acorn-size="18"></i>
+                                        <span class="label">Getting Started</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Menu End -->
+
+                <!-- Page Content Start -->
+                <div class="col">
+                    <!-- Title and Top Buttons Start -->
+                    @yield('content')
+                    <!-- User Cards End -->
+                </div>
+                <!-- Page Content End -->
+            </div>
+        </div>
+    </main>
+
+    <!-- Layout Footer Start -->
+    <footer>
+        <div class="footer-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-6">
+                        <p class="mb-0 text-muted text-medium">Utp {{ date('Y') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Layout Footer End -->
+</div>
+
+
+@include('_layout.scripts')
 </body>
 </html>
