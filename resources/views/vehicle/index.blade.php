b48a90c3bfba5166f2ed9a0919963be6fd49d0b4
Jose Huancilla <jfernandostudent10@gmail.com>
2024-10-29 17:33:42 -0500

    lang and color

--- resources/views/vehicle/index.blade.php
+++ resources/views/vehicle/index.blade.php
@@ -13,12 +13,12 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Vehicles') }}
+                                {{ __('Vehículos') }}
                             </span>
 
                              <div class="float-right">
                                 <a href="{{ route('vehicles.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                                  Crear nuevo vehículo
                                 </a>
                               </div>
                         </div>
@@ -59,11 +59,11 @@
 
                                             <td>
                                                 <form action="{{ route('vehicles.destroy', $vehicle->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('vehicles.show', $vehicle->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('vehicles.edit', $vehicle->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('vehicles.show', $vehicle->id) }}"><i class="fa fa-fw fa-eye"></i> Mostrar</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('vehicles.edit', $vehicle->id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Está seguro?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Está seguro?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> Borrar</button>
                                                 </form>
                                             </td>
                                         </tr>
