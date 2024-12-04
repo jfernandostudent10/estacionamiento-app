0ee3383d7dabaf55399c7687a698bd79340a47bc
Jose Huancilla <jfernandostudent10@gmail.com>
2024-12-03 20:18:13 -0500

    add factory

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -32,6 +33,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Vehicle extends Model
 {
+    use HasFactory;
 	protected $table = 'vehicles';
 
 	protected $casts = [
